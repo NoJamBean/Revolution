e5195d134be6388c9431f6f6cf6f4cfe2a06d0d7
DESKTOP-I62LS2K\soldesk <boris9605@gmail.com>
2025-04-04 16:09:24 +0900

    route53 존 2개 생성

---
+++ Terraform/Route53.tf
@@ -0,0 +1,59 @@
+
+variable "private_domain_name" {
+  default = "backend" # 원하는 도메인명으로 변경
+}
+
+variable "public_domain_name" {
+  default = "frontend" # 원하는 도메인명으로 변경
+}
+
+# 기존 퍼블릭 호스팅 영역이 있으면 사용, 없으면 생성
+# data "aws_route53_zone" "public_zone" {
+#   name         = var.public_domain_name
+#   private_zone = false
+# }
+
+# resource "aws_route53_zone" "public_zone" {
+#   count = length(data.aws_route53_zone.public_zone.id) > 0 ? 0 : 1
+
+#   name = var.public_domain_name
+
+#   lifecycle {
+#     prevent_destroy = true # terraform destroy 시 삭제되지 않도록 보호
+#   }
+# }
+
+# 기존 프라이빗 호스팅 영역이 있으면 사용, 없으면 생성
+data "aws_route53_zone" "private_zone" {
+  name         = var.private_domain_name
+  # private_zone = true
+}
+
+resource "aws_route53_zone" "primary" {
+  count = length(data.aws_route53_zone.private_zone.id) > 0 ? 0 : 1
+  name         = var.private_domain_name
+  # private_zone = true
+  vpc {
+    vpc_id = aws_vpc.vpc.id
+  }
+
+  lifecycle {
+    prevent_destroy = true # terraform destroy 시 삭제되지 않도록 보호
+  }
+}
+
+resource "aws_route53_record" "api" {
+  zone_id = data.aws_route53_zone.private_zone.id
+  name    = "api"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.dotnet_api_server.private_ip] # API 서버의 프라이빗 IP 주소 입력
+}
+
+resource "aws_route53_record" "db" {
+  zone_id = data.aws_route53_zone.private_zone.id
+  name    = "db"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.rds_access_instance.private_ip] # DB 서버의 프라이빗 IP 주소 입력
+}
