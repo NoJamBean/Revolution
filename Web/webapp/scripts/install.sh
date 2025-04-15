b9fb9df581de5d89ea6b33c2bb4c15cd3bdbbaf7
songhyunseop <gustjq99455352@gmail.com>
2025-04-15 23:53:12 +0900

    code please please

--- Web/webapp/scripts/install.sh
+++ Web/webapp/scripts/install.sh
@@ -4,12 +4,16 @@ echo "[AfterInstall] 의존성 설치 시작"
 
 # Node.js 수동 설치
 echo "[AfterInstall] Node.js 18.17.1 설치 중"
+
 sudo wget -nv https://d3rnber7ry90et.cloudfront.net/linux-x86_64/node-v18.17.1.tar.gz
 sudo mkdir -p /usr/local/lib/node
 sudo tar -xf node-v18.17.1.tar.gz
 sudo mv node-v18.17.1 /usr/local/lib/node/nodejs
 
 sudo rm -f node-v18.17.1.tar.gz
+
+export NODEJS_HOME=/usr/local/lib/node/nodejs
+export PATH=$NODEJS_HOME/bin:$PATH
 
 cat << 'EOF' | sudo tee /etc/profile.d/node.sh > /dev/null
 export NODEJS_HOME=/usr/local/lib/node/nodejs
