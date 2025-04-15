cef7b7b7fcbaf7cc9d90684798f08d0cce2b977d
songhyunseop <gustjq99455352@gmail.com>
2025-04-15 17:51:28 +0900

    마지막 수정

--- Web/webapp/scripts/install.sh
+++ Web/webapp/scripts/install.sh
@@ -2,34 +2,33 @@
 
 echo "[AfterInstall] 의존성 설치 시작"
 
+# Node.js 18.17.1 직접 설치
+if ! command -v node &> /dev/null; then
+  echo "[AfterInstall] Node.js 바이너리 직접 설치 중"
+  wget -nv https://d3rnber7ry90et.cloudfront.net/linux-x86_64/node-v18.17.1.tar.gz
+  mkdir -p /usr/local/lib/node
+  tar -xf node-v18.17.1.tar.gz
+  mv node-v18.17.1 /usr/local/lib/node/nodejs
+
+  echo "export NODEJS_HOME=/usr/local/lib/node/nodejs" >> /etc/profile.d/node.sh
+  echo 'export PATH=$NODEJS_HOME/bin:$PATH' >> /etc/profile.d/node.sh
+  chmod +x /etc/profile.d/node.sh
+  source /etc/profile.d/node.sh
+fi
+
+# pm2, yarn 설치
+npm install -g pm2
+npm install -g yarn
+
+
+# 📌 이제 프로젝트 폴더로 이동
 cd /home/ec2-user/app
 
-
-# node 설치 - 18.17.1v
-export NVM_DIR="/home/ec2-user/.nvm"
-if [ ! -d "$NVM_DIR" ]; then
-  echo "[AfterInstall] nvm 설치 중"
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-fi
-
-# nvm 로딩
-export NVM_DIR="/home/ec2-user/.nvm"
-source "$NVM_DIR/nvm.sh"
-
-# Node.js 18.17.1 설치 및 사용
-nvm install 18.17.1
-nvm use 18.17.1
-nvm alias default 18.17.1
-
-
-# pm2 없으면 설치
-if ! command -v pm2 &> /dev/null; then
-  echo "[AfterInstall] pm2 설치 중"
-  sudo npm install -g pm2
-fi
-
+# 의존성 설치
 yarn install --frozen-lockfile
 
+# 배포 스크립트 실행 권한 부여
 chmod +x scripts/*.sh
 
 echo "[AfterInstall] 완료"
+
