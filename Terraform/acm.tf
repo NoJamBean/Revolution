6e7ab8635869fa7234748474e436c00ae6acd319
Bean <gnfkqh@gmail.com>
2025-04-25 17:22:52 +0900

    env입력 수정

---
+++ Terraform/acm.tf
@@ -0,0 +1,17 @@
+resource "aws_acm_certificate" "alb_cert" {
+  domain_name       = "1bean.shop"
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "www.1bean.shop"
+  ]
+
+  tags = {
+    Name = "alb-cert"
+  }
+}
+
+resource "aws_acm_certificate_validation" "alb_cert" {
+  certificate_arn         = aws_acm_certificate.alb_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.alb_cert_validation : record.fqdn]
+}
--- Web/webapp/scripts/start.sh
+++ Web/webapp/scripts/start.sh
@@ -1,37 +1,6 @@
 #!/bin/bash
 set -e
 
-# echo "[ApplicationStart] PM2로 Next.js 앱 실행 시작"
-
-# export NODEJS_HOME=/usr/local/lib/node/nodejs
-# export PATH=$NODEJS_HOME/bin:$PATH
-
-# cd /home/ec2-user/app
-
-# APP_NAME="revolution-app"
-
-# sudo yum install -y libcap
-# sudo setcap 'cap_net_bind_service=+ep' $NODEJS_HOME/bin/node
-
-# # 이전 실행된 pm2 프로세스 종료 (있으면)
-# pm2 delete "$APP_NAME" || echo "[ApplicationStart] 기존 pm2 앱 없음"
-
-
-# # 앱 실행 (yarn start → next start)
-# PORT=80 pm2 start yarn --name "$APP_NAME" -- start
-
-# # PM2 상태 저장 (재부팅 시 자동 복구용)
-# pm2 save
-
-# # healthCheck 서버 종료 전 next 서버 on 체크
-# until curl -s -o /dev/null -w "%{http_code}" http://localhost/ | grep -q "200"; do
-#   echo "Waiting for Next.js server to become healthy..."
-#   sleep 3
-# done
-
-# echo "[ApplicationStart] PM2 앱 실행 완료"
-
-
 echo "[ApplicationStart] PM2로 Next.js 앱 실행 시작"
 
 export NODEJS_HOME=/usr/local/lib/nodejs/node-v18.17.1-linux-x64
