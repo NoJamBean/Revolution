c84a34c6bc0e7ce77e625c1e1f31a038443491bb
Bean <gnfkqh@gmail.com>
2025-04-27 01:40:48 +0900

    웹소켓 수정

--- Terraform/userdatas/web_server.sh
+++ Terraform/userdatas/web_server.sh
@@ -57,6 +57,14 @@ server {
     listen 80;
     server_name www.1bean.shop;
 
+    location = /api/log {
+        proxy_pass http://localhost:3000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
     # API 프록시 (프라이빗 ALB)
     location /api/ {
         proxy_pass http://alb.backend.internal/api/;
