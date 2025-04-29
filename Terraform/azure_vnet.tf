e9f9cf85080405a374df2cf36b585850859c924b
DESKTOP-I62LS2K\soldesk <boris9605@gmail.com>
2025-04-29 15:04:08 +0900

    Azure 컨테이너 오토스케일링 그룹 추가

--- Terraform/azure_vnet.tf
+++ Terraform/azure_vnet.tf
@@ -48,3 +48,68 @@ resource "azurerm_app_service" "app_service" {
     environment = "production"
   }
 }
+
+resource "azurerm_monitor_autoscale_setting" "autoscale" {
+  name                = "autoscale-webapp"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  target_resource_id  = azurerm_service_plan.asp.id
+  enabled             = true
+
+  profile {
+    name = "default"
+
+    capacity {
+      minimum = "1"
+      maximum = "3"
+      default = "1"
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.asp.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 70
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_app_service.app_service.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 30
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+  }
+
+  notification {
+    email {
+      send_to_subscription_administrator    = true
+      send_to_subscription_co_administrator = false
+    }
+  }
+}
