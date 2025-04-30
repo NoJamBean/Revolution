64174c18851d02a9eb3821f385a3636cbf783d99
Bean <gnfkqh@gmail.com>
2025-05-01 02:22:09 +0900

    오류 수정

--- Terraform/azure_container.tf
+++ Terraform/azure_container.tf
@@ -38,12 +38,9 @@ resource "azurerm_linux_web_app" "app_service" {
 }
 
 # Staging 슬롯 생성
-resource "azurerm_web_app_slot" "staging_slot" {
+resource "azurerm_linux_web_app_slot" "staging_slot" {
   name                = "staging"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
-  app_service_name    = azurerm_linux_web_app.app_service.name
+  app_service_id = azurerm_linux_web_app.app_service.id
 
   site_config {}
 }
