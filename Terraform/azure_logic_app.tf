0e7c2812e231cb26b254004c8f01955246075001
Bean <gnfkqh@gmail.com>
2025-05-01 17:50:21 +0900

    애저 로직 앱 추가

---
+++ Terraform/azure_logic_app.tf
@@ -0,0 +1,15 @@
+resource "azurerm_logic_app_workflow" "main" {
+  name                = "main-logic-app"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  workflow_schema     = "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#"
+
+  # Logic Apps 정의 (메트릭 가져오기 및 S3로 데이터 전송)
+  workflow_parameters = {
+    "subscription_id"      = "29ec5d86-72b1-4f74-9d88-711d967e3b86"
+    "resource_group_name"  = azurerm_resource_group.main.name
+    "app_service_name"     = azurerm_linux_web_app.app_service.name
+    "metric_names"         = "CpuPercentage,Requests"
+    "bucket_name"          = "bet-application-total-logs"
+  }
+}
