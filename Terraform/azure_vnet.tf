04c247b8a6a0d0e11ff6a29521581fd6360b9383
Bean <gnfkqh@gmail.com>
2025-05-01 19:53:21 +0900

    테스트

--- Terraform/azure_vnet.tf
+++ Terraform/azure_vnet.tf
@@ -10,10 +10,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.main.name
 
   dns_servers = [
-    # "ns-1536.awsdns-00.co.uk.",
-    # "ns-0.awsdns-00.com.,",
-    # "ns-1024.awsdns-00.org.",
-    # "ns-512.awsdns-00.net.",
+    "10.0.0.2",
     "8.8.8.8"
   ]
 }
