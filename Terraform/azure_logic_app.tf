7334c415766d1588f7b8bfac3d8016587719ab82
Bean <gnfkqh@gmail.com>
2025-05-01 18:23:20 +0900

    다시 수정

--- Terraform/azure_logic_app.tf
+++ Terraform/azure_logic_app.tf
@@ -7,8 +7,8 @@ resource "azurerm_logic_app_workflow" "main" {
   # Logic Apps 정의 (메트릭 가져오기 및 S3로 데이터 전송)
   workflow_parameters = jsonencode({
     "subscription_id"      = "29ec5d86-72b1-4f74-9d88-711d967e3b86",
-    "resource_group_name"  = azurerm_resource_group.main.name,
-    "app_service_name"     = azurerm_linux_web_app.app_service.name,
+    "resource_group_name"  = "${azurerm_resource_group.main.name}",
+    "app_service_name"     = "${azurerm_linux_web_app.app_service.name}",
     "metric_names"         = "CpuPercentage,Requests",
     "bucket_name"          = "bet-application-total-logs"
     "s3_key_prefix"        = "azure/metrics_${timestamp()}.json"
