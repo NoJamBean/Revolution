84818d6c1cef3190ca21a43820e5d13ef5e18d9f
Bean <gnfkqh@gmail.com>
2025-05-01 20:22:31 +0900

    수정본

--- Terraform/azure_container.tf
+++ Terraform/azure_container.tf
@@ -39,7 +39,7 @@ resource "azurerm_linux_web_app" "app_service" {
 
 # Staging 슬롯 생성
 resource "azurerm_linux_web_app_slot" "staging_slot" {
-  name                = "staging"
+  name                = azurerm_linux_web_app.app_service.name
   app_service_id = azurerm_linux_web_app.app_service.id
 
   site_config {
