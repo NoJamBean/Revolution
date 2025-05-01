c5f9e0a3fc2fb7681bc2513bb450b5b82806558a
Bean <gnfkqh@gmail.com>
2025-05-02 01:02:07 +0900

    주관이형 슛

--- Terraform/userdatas/web_server.sh
+++ Terraform/userdatas/web_server.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
-# set -e
+set -e
 
-# apt-get update -y
-# apt-get upgrade -y
+apt-get update -y
+apt-get upgrade -y
 
-# apt-get install -y \
-#     ca-certificates \
-#     curl \
-#     gnupg \
-#     lsb-release
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-# # 도커 공식 GPG 키 등록
-# install -m 0755 -d /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-#     gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# 도커 공식 GPG 키 등록
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+    gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-# chmod a+r /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
-# # 저장소 추가
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) \
-#   signed-by=/etc/apt/keyrings/docker.gpg] \
-#   https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+# 저장소 추가
+echo \
+  "deb [arch=$(dpkg --print-architecture) \
+  signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# apt-get update -y
-# apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# systemctl enable docker
-# systemctl start docker
+systemctl enable docker
+systemctl start docker
 
-# # 3. Docker Compose 플러그인 설치 (v2 방식)
-# mkdir -p /home/ubuntu/.docker/cli-plugins/
-# curl -SL "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
-#   -o /home/ubuntu/.docker/cli-plugins/docker-compose
-# chmod +x /home/ubuntu/.docker/cli-plugins/docker-compose
-# chown -R ubuntu:ubuntu /home/ubuntu/.docker
+# 3. Docker Compose 플러그인 설치 (v2 방식)
+mkdir -p /home/ubuntu/.docker/cli-plugins/
+curl -SL "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
+  -o /home/ubuntu/.docker/cli-plugins/docker-compose
+chmod +x /home/ubuntu/.docker/cli-plugins/docker-compose
+chown -R ubuntu:ubuntu /home/ubuntu/.docker
 
 # # 4. CodeDeploy Agent 설치
 # apt-get install -y ruby wget
