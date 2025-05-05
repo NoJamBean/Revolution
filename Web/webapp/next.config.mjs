31857e25cc5267c98d415e401d9a2ec3554074ba
Bean <gnfkqh@gmail.com>
2025-05-05 21:31:25 +0900

    오류수정

--- Web/webapp/next.config.mjs
+++ Web/webapp/next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
+
+  async rewrites() {
+    return [
+      {
+        source: '/ws',        // 클라이언트에서 /ws 로 요청하면
+        destination: '/api/ws' // 내부적으로 /api/ws 로 포워딩
+      }
+    ];
+  },
 };
 
 export default nextConfig;
