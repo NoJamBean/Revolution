1e85fd1a3fc07d3c30a4db216e4ade997f889117
Bean <gnfkqh@gmail.com>
2025-05-06 23:12:39 +0900

    하 진짜 멍청한 지피티

--- Web/webapp/server.mjs
+++ Web/webapp/server.mjs
@@ -45,7 +45,7 @@ async function main() {
   server.on('upgrade', (req, socket, head) => {
     if (req.url.startsWith('/ws')) {
       proxy.ws(req, socket, head, {
-        target: 'http://alb.backend.internal/ws',
+        target: 'http://alb.backend.internal',
       });
     }
   });
