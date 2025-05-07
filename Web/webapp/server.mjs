254cfaac95a7f75e0973edbfa1cd7e457b9b0ac8
Bean <gnfkqh@gmail.com>
2025-05-07 23:10:11 +0900

    롤백

--- Web/webapp/server.mjs
+++ Web/webapp/server.mjs
@@ -3,7 +3,6 @@ import express from 'express';
 import http from 'http';
 import httpProxy from 'http-proxy';
 import bodyParser from 'body-parser';
-import { uploadToS3 } from './utils/s3uploader.js'; // S3 업로더 경로 확인
 
 const dev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 3000;
@@ -18,42 +17,6 @@ const proxy = httpProxy.createProxyServer({
 
 nextApp.prepare().then(() => {
   const app = express();
-
-  app.use((req, res, next) => {
-    res.on('finish', () => {
-      const candidateIP = Array.isArray(req.headers['x-forwarded-for'])
-      ? req.headers['x-forwarded-for'][0]
-      : typeof req.headers['x-forwarded-for'] === 'string'
-      ? req.headers['x-forwarded-for']
-      : typeof req.socket.remoteAddress === 'string'
-      ? req.socket.remoteAddress
-      : '';
-
-      const sourceIPAddress = candidateIP
-      .split(',')[0]
-      .replace(/:\d+$/, '');
-
-      const log = {
-        eventSource: 'webapp.example.com',
-        awsRegion: 'ap-northeast-2',
-        eventTime: new Date().toISOString(),
-        eventName: 'HttpAccess',
-        requestParameters: {
-          httpMethod: req.method,
-          requestPath: req.originalUrl,
-          statusCode: res.statusCode,
-        },
-        sourceIPAddress: sourceIPAddress,
-        userAgent: req.headers['user-agent'] || '',
-      };
-  
-      uploadToS3(log).catch((err) =>
-        console.error('[❌ S3 로그 업로드 실패]', err.message)
-      );
-    });
-  
-    next();
-  });
 
   app.use('/api', (req, res) => {
     proxy.web(req, res, {
@@ -108,3 +71,4 @@ nextApp.prepare().then(() => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
   });
 });
+
