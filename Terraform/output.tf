3bd9c6b4744a9f7cd74fd73b5a0deb57f170e687
Bean <gnfkqh@gmail.com>
2025-05-10 00:45:33 +0900

    불필요한 output 삭제

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -24,34 +24,34 @@ output "cluster_endpoint" {
   value = aws_eks_cluster.main.endpoint
 }
 
-output "kubeconfig" {
-  value = <<EOT
-apiVersion: v1
-clusters:
-- cluster:
-    server: ${aws_eks_cluster.main.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.main.certificate_authority[0].data}
-  name: ${aws_eks_cluster.main.name}
-contexts:
-- context:
-    cluster: ${aws_eks_cluster.main.name}
-    user: aws
-  name: ${aws_eks_cluster.main.name}
-current-context: ${aws_eks_cluster.main.name}
-kind: Config
-preferences: {}
-users:
-- name: aws
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1beta1
-      command: aws
-      args:
-        - "eks"
-        - "get-token"
-        - "--region"
-        - "ap-northeast-2"
-        - "--cluster-name"
-        - "${aws_eks_cluster.main.name}"
-EOT
-}
+# output "kubeconfig" {
+#   value = <<EOT
+# apiVersion: v1
+# clusters:
+# - cluster:
+#     server: ${aws_eks_cluster.main.endpoint}
+#     certificate-authority-data: ${aws_eks_cluster.main.certificate_authority[0].data}
+#   name: ${aws_eks_cluster.main.name}
+# contexts:
+# - context:
+#     cluster: ${aws_eks_cluster.main.name}
+#     user: aws
+#   name: ${aws_eks_cluster.main.name}
+# current-context: ${aws_eks_cluster.main.name}
+# kind: Config
+# preferences: {}
+# users:
+# - name: aws
+#   user:
+#     exec:
+#       apiVersion: client.authentication.k8s.io/v1beta1
+#       command: aws
+#       args:
+#         - "eks"
+#         - "get-token"
+#         - "--region"
+#         - "ap-northeast-2"
+#         - "--cluster-name"
+#         - "${aws_eks_cluster.main.name}"
+# EOT
+# }
